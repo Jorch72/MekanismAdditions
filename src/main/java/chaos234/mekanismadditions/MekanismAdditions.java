1d6ce93db829d7c957c59023dc38c7f73bfd7504
Chaos234 <info@deltanet-production.de>
2016-09-08 20:44:37 +0200

    initial commit

--- src/main/java/chaos234/mekanismadditions/MekanismAdditions.java
+++ src/main/java/chaos234/mekanismadditions/MekanismAdditions.java
@@ -3,33 +3,31 @@ package chaos234.mekanismadditions;
 import chaos234.mekanismadditions.common.CommonProxy;
 import chaos234.mekanismadditions.common.MekanismAdditionsFluids;
 import chaos234.mekanismadditions.common.MekanismAdditionsItems;
+import chaos234.mekanismadditions.common.recipe.ShapedMekanismAdditionsRecipe;
 import mekanism.api.ItemRetriever;
-import mekanism.api.util.*;
-import mekanism.api.MekanismConfig;
 
 import mekanism.api.gas.GasRegistry;
 import mekanism.api.gas.GasStack;
 import mekanism.api.recipe.RecipeHelper;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.item.crafting.ShapelessRecipes;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.security.auth.login.Configuration;
+import java.lang.Object;
 
 /**
  * Created by Marcel on 08.09.2016.
  */
-@Mod(modid = MekanismAdditions.MODID, name = MekanismAdditions.MODNAME, version = MekanismAdditions.MODVERSION, dependencies = "required-after:Mekanism@[9.2.0.292]", useMetadata = true)
+@Mod(modid = MekanismAdditions.MODID, name = MekanismAdditions.MODNAME, version = MekanismAdditions.MODVERSION, dependencies = "required-after:Mekanism", useMetadata = true)
 public class MekanismAdditions {
     /* Define some default vars */
     public static final String MODID = "mekanismadditions";
@@ -41,7 +39,7 @@ public class MekanismAdditions {
     public static Logger maLogger = LogManager.getLogger("Mekanism Additions");
 
     /* Mekanism Additions proxy instance */
-    @SidedProxy(clientSide = "mekanismadditions.client.ClientProxy", serverSide = "mekanismadditions.common.CommonProxy")
+    @SidedProxy(clientSide = "chaos234.mekanismadditions.client.ClientProxy", serverSide = "chaos234.mekanismadditions.common.CommonProxy")
     public static CommonProxy proxy;
 
     /* Mekanism Additions mod instance */
@@ -56,14 +54,12 @@ public class MekanismAdditions {
     }
 
     public void addRecipes() {
-
-        ItemStack controlCircuit = ItemRetriever.getItem("ControlCircuit");
-
-        CraftingManager.getInstance().getRecipeList().add(new ShapedRecipes(3, 3, new ItemStack(MekanismAdditionsItems.InformationTab, 1),
-                " C ", "CBC", " C ", Character.valueOf('C'), stack , Character.valueOf('B'), Items.BOOK
+        Object bcc = ItemRetriever.getItem("ControlCircuit");
+        CraftingManager.getInstance().getRecipeList().add(new ShapedMekanismAdditionsRecipe(new ItemStack(MekanismAdditionsItems.InformationTab,1),
+                " C ", "CBC", " C ", Character.valueOf('C'), new ItemStack((Item)bcc,1,0), Character.valueOf('B'), Items.BOOK
         ));
 
-        //secound way for heavy water
+        //second way for heavy water
             // RecipeHelper.addChemicalInfuserRecipe(new GasStack(GasRegistry.getGas("hydrogen"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfidgas"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfidsnd"), 1));
         RecipeHelper.addChemicalInfuserRecipe(new GasStack(GasRegistry.getGas("hydrogen"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfid"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfidsnd"), 1));
         RecipeHelper.addChemicalInfuserRecipe(new GasStack(GasRegistry.getGas("hydrogen"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfidsnd"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfidrd"), 1));
@@ -74,6 +70,13 @@ public class MekanismAdditions {
         RecipeHelper.addChemicalWasherRecipe(new GasStack(GasRegistry.getGas("enricheddihydrogensulfidrd"), 1), new GasStack(GasRegistry.getGas("enrichedwaterrd"), 1));
 
         RecipeHelper.addChemicalWasherRecipe(new GasStack(GasRegistry.getGas("sulfurDioxideGas"), 1), new GasStack(GasRegistry.getGas("enricheddihydrogensulfid"), 1));
-
     }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        maLogger.info("[Mekansim Additions]: Adding integration to Mekanism ...");
+        addRecipes();
+        maLogger.info("[Mekansim Additions]: Adding complete.");
+    }
+
 }
